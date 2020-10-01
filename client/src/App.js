5b45d72add974933e91df1c995020df235b1d75d
Romarjo Balukja <mbalukja1@gmail.com>
2020-10-01 16:51:18 +0200

    Leksioni 12 - Implement Router

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,22 @@ import AppNavbar from './components/navigation/AppNavbar';
 import { Provider } from "react-redux";
 import store from "./store";
 import Item from "./components/items/Item";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
 
   render() {
     return (
         <Provider store={store}>
-            <div className="App">
-                <AppNavbar>
-                    <Item />
-                </AppNavbar>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <AppNavbar>
+                        <Switch>
+                            <Route exact path={"/"} component={Item}/>
+                        </Switch>
+                    </AppNavbar>
+                </div>
+            </BrowserRouter>
         </Provider>
     );
   }
